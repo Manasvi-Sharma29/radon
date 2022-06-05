4ff2f3c169c50b3dab7d795648628ac4c57db2e9
Manasvi-Sharma29 <manasvi.sharma2929@gmail.com>
2022-06-05 21:32:52 +0530

    This is the assignment of mongoDB

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,31 @@
 const UserModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
+// const createUser= async function (req, res) {
+//     let data= req.body
+//     let savedData= await UserModel.create(data)
+//     res.send({msg: savedData})
+// }
+
+// const getUsersData= async function (req, res) {
+//     let allUsers= await UserModel.find()
+//     res.send({msg: allUsers})
+// }
+
+// module.exports.createUser= createUser
+// module.exports.getUsersData= getUsersData
+
+// ASSIGNMENT
+
+const createBook = async function(req, res){
+    let data = req.body
+    let savedData = await UserModel.create(data)
     res.send({msg: savedData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+const getBook = async function(req, res){
+    let allBooks = await UserModel.find()
+    res.send({msg: allBooks})
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+module.exports.createBook = createBook
+module.exports.getBook = getBook
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,8 +7,16 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
+
+// ASSIGNMENT
+
+router.post("/createBook" , UserController.createBook)
+
+router.get("/getBook" , UserController.getBook)
+
+
 
 module.exports = router;
