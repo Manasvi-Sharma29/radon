4cc78bf832670d4d035f0029b8fc9db495e444fe
Manasvi-Sharma29 <manasvi.sharma2929@gmail.com>
2022-06-10 22:20:52 +0530

    basic assignment based on middileware

--- src/routes/route.js
+++ src/routes/route.js
@@ -16,7 +16,7 @@ const mid1 = function(req,res,next){
     let time = new Date()
     let ipAddress = req.ip
     let routeUsed = req.route.path
-    console.log("hi! I'm a middileware, my name is mid1 and below is what Igit logged")
+    console.log("hi! I'm a middileware, my name is mid1 and below is what I logged")
     console.log(time, ipAddress, routeUsed)
     
     next()
