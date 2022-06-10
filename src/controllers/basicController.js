c40a7eaa64a463a77f752ceb7107ebd810d2312d
Manasvi-Sharma29 <manasvi.sharma2929@gmail.com>
2022-06-10 22:10:11 +0530

    basic assignment of middleware

---
+++ src/controllers/basicController.js
@@ -0,0 +1,6 @@
+const basicCode = async function(req,res){
+    let a = "done"
+    res.send({msg: a})
+}
+
+module.exports.basicCode = basicCode 
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,60 +3,26 @@ const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+const basicController = require("../controllers/basicController")
+const commonMW = require ("../middlewares/commonMiddlewares");
+const { route } = require('express/lib/application');
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-
-
-
-router.post("/createBook", BookController.createBook  )
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
 
 
+const mid1 = function(req,res,next){
+    let time = new Date()
+    let ipAddress = req.ip
+    let routeUsed = req.route.path
+    console.log("hi! I'm a middileware, my name is mid1 and below is what Igit logged")
+    console.log(time, ipAddress, routeUsed)
+    
+    next()
+}
 
-
-// router.post("/createUser", UserController.createUser  )
-// router.get("/getUsersData", UserController.getUsersData)
-
-
-// const mid1= function ( req, res, next) {
-//     console.log("Hi I am a middleware named Mid1")
-//     // logic
-//     let loggedIn = false
-
-//     if (loggedIn== true) { 
-//         console.log( "OK LOGGED IS IS TRUE NOW")
-//         next ()
-//     }
-//     else {
-//         res.send ("Please login or register")
-//     }
-// }
-
-// // e.g. restricted and open-to-all API's can be handled like below now:
-// router.get('/homePage', mid1, UserController.feeds)
-// router.get('/profileDetails', mid1, UserController.profileDetails)
-// router.get('/friendList', mid1, UserController.friendList)
-// router.get('/changePassword', mid1, UserController.changePassword)
-
-// router.get('/termsAndConditions',  UserController.termsAndConditions)
-// router.get('/register',  UserController.register)
-
-
-
-
-
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
-
-
-
-// router.get("/basicRoute2", commonMW.mid1, UserController.basicCode2)
-// router.get("/basicRoute3", commonMW.mid2, UserController.basicCode3)
-// router.get("/basicRoute4", commonMW.mid1, commonMW.mid4, UserController.basicCode4)
-
-
+router.get('/basicRoute', mid1, basicController.basicCode )
 
 
 module.exports = router;
