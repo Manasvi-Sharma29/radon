ac42b44804139ab4a1f2f47802d5d35b7522e75b
Manasvi Sharma <106535031+Manasvi-Sharma29@users.noreply.github.com>
2022-08-22 02:42:42 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -24,5 +24,7 @@ router.post('/createAuthor', authorController.createAuthor)
 router.post('/createPublisher', publisherController.createPublisher)
 router.post('/createBook' , bookController.createBook)
 router.get('/getBooksData' , bookController.getBooksData)
+router.put("/books", bookController.updateBookCover);
+router.put("/updatePrice", bookController.updatePrice);
 
 module.exports = router;
