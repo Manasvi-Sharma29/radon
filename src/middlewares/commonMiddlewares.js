1d8299d277212ef52ecd84d72a509d22fc2278be
Manasvi Sharma <106535031+Manasvi-Sharma29@users.noreply.github.com>
2022-08-27 01:20:02 +0530

    Update commonMiddlewares.js

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,9 +1,5 @@
 // ASSIGNMENT
 
-const orderModel = require("../models/orderModel")
-const productModel = require("../models/productModel")
-const userModel = require("../models/userModel")
-
 const mid1 = function(req, res, next){
     let header = req.headers["isFreeAppUser"]
     if(!header){
@@ -13,22 +9,9 @@ const mid1 = function(req, res, next){
     if(!header){
         res.send({msg: "The request is missing a mandatory header"})
     }
+    req.isFreeAppUser = isfreeappuser;
+    next();
 }
-const mid2 = async function(req, res, next){
-    
-}
 
-
-
-// const mid3 = async function(req,res,next){
-//     let productId = req.body.product_id
-//     let userId = req.body.user_id
-//     let bal = await userModel.findById({userId}).select({})
-//     if(bal.balance >= 100){
-//         let checkPrice = await productModel.findById({productId}).select({price:1, _id:0})
-//         let newBalance = bal.price.updateOne({_id:userId},{$inc:{price:-checkPrice.price}})
-//     }
-//     next()
-// }
 
 module.exports.mid1 = mid1
